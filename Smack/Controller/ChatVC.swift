60d0c7e7a8ce5ac8b56dc5e4dc3ec40a3781d966
Phuc Le Dien <phucledien@gmail.com>
2017-08-23 11:49:45 +0700

    Fixed linked library

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Phuc Le Dien on 8/22/17.
+//  Copyright © 2017 phucledien. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+    
+}
