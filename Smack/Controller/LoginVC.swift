3affd9ddd0ff3ebe0869a28a1bd5e59470d14587
Phuc Le Dien <phucledien@gmail.com>
2017-08-24 12:24:15 +0700

    Added Login View

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Phuc Le Dien on 8/24/17.
+//  Copyright © 2017 phucledien. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Phuc Le Dien on 8/24/17.
+//  Copyright © 2017 phucledien. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
