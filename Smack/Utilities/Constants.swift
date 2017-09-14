6d77e81b9b60024535db6d456b6a276b2d3580df
Phuc Le Dien <phucledien@gmail.com>
2017-09-14 23:19:06 +0700

    Added login function

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -15,6 +15,7 @@ let BASE_URL = "https://phucldchatty.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register/"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 
 // Colors
 let smackPurplePlaceholder = #colorLiteral(red: 0.3254901961, green: 0.4196078431, blue: 0.7764705882, alpha: 0.5)
@@ -34,5 +35,10 @@ let USER_EMAIL = "userEmail"
 
 // Headers
 let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
+
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.authToke)",
     "Content-Type": "application/json; charset=utf-8"
 ]
